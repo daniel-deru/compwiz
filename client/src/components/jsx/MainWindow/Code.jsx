74a401f40ec1509be90c5414dccd508496ed495c
daniel <danielderu3@gmail.com>
2021-06-28 16:51:25 +0200

    switched to use codemirror for code editor

--- client/src/components/jsx/MainWindow/Code.jsx
+++ client/src/components/jsx/MainWindow/Code.jsx
@@ -3,6 +3,15 @@ import { useEffect, useState, useRef } from "react"
 
 //import stylesheets
 import "../../css/MainWindowCSS/Code.css"
+import "codemirror/lib/codemirror.css"
+import "codemirror/theme/material.css"
+
+
+//editor imports
+import "codemirror/mode/javascript/javascript"
+
+// import the code editor
+import { Controlled as Editor} from "react-codemirror2"
 
 
 
@@ -11,34 +20,31 @@ import "../../css/MainWindowCSS/Code.css"
 const Code = () => {
     const codeRef = useRef()
     const [lines, setLines] = useState([""])
+    const [code, setCode] = useState("")
     
-
-    const getCode = (e) => {
-        const characterCode = e.target.textContent
-        const getNewLine = /\r*\n/g
-        const no_of_lines = characterCode.split(getNewLine)
-        setLines(no_of_lines)
-        console.log(no_of_lines)
-        // console.log(characterCode)
+    function handleChange(editor, data, value){
+        setCode(value)
     }
 
-    // const renderLines = (currentLines) => {
-    //     for(let i = 0; i <= currentLines; i++){
-    //         return <li></li>
-    //     }
-    // }
-    // console.log(lines)
+
     return (
-        <div ref={codeRef} id="coding-area"  >
-            <div id="line-no">
-                <ol>
-                    {/* {lines.map(elements => {
-                        return <li></li>
-                    })} */}
-                </ol>
-            </div>
-            <div id="main-code" contentEditable='true' onInput={(e) => getCode(e)}></div>
-            {/* <textarea name="main-code" id="main-code" cols="30" rows="10" onChange={(e) => getCode(e)}></textarea> */}
+        <div id="coding-area" >
+            {/* <div id="main-code"> */}
+                <Editor
+                    onBeforeChange={handleChange}
+                    options={{
+                        mode: "javascript",
+                        theme: "material",
+                        lineNumbers: true,
+                        lineWrapping: true,
+                        lint: true
+                    }}
+                    value={code}
+                    className="editor"
+                    
+                
+                />
+            {/* </div> */}
         </div>
     )
 }
